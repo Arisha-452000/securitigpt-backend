2fbe5fc9a04c693f7286a5e41be9801c9ff2f6bf
Arisha-452000 <arisha.a20004@gmail.com>
2026-04-02 04:13:49 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,8 @@ export PYTHONPATH=/opt/render/project/src/backend:$PYTHONPATH
 
 # Start the FastAPI application
 cd /opt/render/project/src/backend
+
+# Automatically recreate admin user in case the database was wiped
+python create_admin.py
+
 python render_start.py
